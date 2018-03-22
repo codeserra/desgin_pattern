a49687995441c91b9949ae02aa9366ee23dd08b6
codeserra <36965166+codeserra@users.noreply.github.com>
2018-03-22 10:12:57 +0530

    Create Calculator_design.cpp

---
+++ Questions/Calculator_design.cpp
@@ -0,0 +1,12 @@
+// Asked me to in one of the interview.
+// Design a calculator. 
+// Ans = (2 +(4- (6*7)))
+//         
+//          +
+//        /   \
+//      2      -
+//           /   \
+//         4      +
+//              /   \
+//            6       7
+//  
